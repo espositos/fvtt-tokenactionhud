c0daa7c486458c435ed5f7a31f2d3f79cff13e72
Hamish <hamish.martin@gmail.com>
2020-07-19 21:32:53 +0200

    Misnamed method

--- scripts/actions/exampleActionHandler.js
+++ scripts/actions/exampleActionHandler.js
@@ -6,7 +6,7 @@ export class ExampleActionHandler extends ActionHandler {
     }
 
     /** @override */
-    async dobuildActionList(token) {
+    async doBuildActionList(token) {
         let result = this.initializeEmptyActionList();
 
         if (!token)
