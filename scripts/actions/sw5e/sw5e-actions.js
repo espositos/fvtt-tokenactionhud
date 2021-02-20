ca0de7aa59c02890cae6ac39fcf87a9ecb81f2c7
Hamish <hamish.martin@gmail.com>
2021-02-20 20:08:06 +0100

    SW5e add conditions support

--- scripts/actions/sw5e/sw5e-actions.js
+++ scripts/actions/sw5e/sw5e-actions.js
@@ -37,12 +37,16 @@ export class ActionHandlerSw5e extends ActionHandler {
 		let classFeatures = this._getClassFeaturesList(actor, tokenId);
         let skills = this._getSkillsList(actor.data.data.skills, tokenId);
         let utility = this._getUtilityList(actor, tokenId);
+		let conditions;
+        if (settings.get('showConditionsCategory'))
+            conditions = this._getConditionsList(actor, tokenId);
 
         let itemsTitle = this.i18n('tokenactionhud.inventory');
         let powersTitle = this.i18n('tokenactionhud.powers');
         let featsTitle = this.i18n('tokenactionhud.features');
 		let classFeaturesTitle = this.i18n('tokenactionhud.classFeatures');
         let skillsTitle = this.i18n('tokenactionhud.skills');
+		let conditionsTitle = this.i18n('tokenactionhud.conditions');
         
         this._combineCategoryWithList(result, itemsTitle, items);
         this._combineCategoryWithList(result, powersTitle, powers);
@@ -57,6 +61,7 @@ export class ActionHandlerSw5e extends ActionHandler {
         
         this._combineCategoryWithList(result, savesTitle, saves);
         this._combineCategoryWithList(result, checksTitle, checks);
+		this._combineCategoryWithList(result, conditionsTitle, conditions);
     
         let utilityTitle = this.i18n('tokenactionhud.utility');
         this._combineCategoryWithList(result, utilityTitle, utility);
@@ -83,6 +88,9 @@ export class ActionHandlerSw5e extends ActionHandler {
         this._addMultiAbilities(list, tokenId, 'saves', savesTitle, 'abilitySave');
         this._addMultiAbilities(list, tokenId, 'checks', checksTitle, 'abilityCheck');
 
+		if (settings.get('showConditionsCategory'))
+            this._addMultiConditions(list, tokenId);
+		
         this._addMultiUtilities(list, tokenId, actors);
     }
     
@@ -528,6 +536,68 @@ export class ActionHandlerSw5e extends ActionHandler {
     }
 
 
+	/** CONDITIONS **/
+	
+	    /** @private */
+    _getConditionsList(actor, tokenId) {
+        let result = this.initializeEmptyCategory('conditions');
+        this._addConditionsSubcategory(actor, tokenId, result);
+        return result;
+    }
+	
+	    /** @private */
+    _addConditionsSubcategory(actor, tokenId, category) {
+        const macroType = 'condition';
+
+        const availableConditions = CONFIG.statusEffects.filter(condition => condition.id !== '');
+
+        if (!availableConditions)
+            return;
+
+        let conditions = this.initializeEmptySubcategory();
+
+        availableConditions.forEach(c => {
+            const name = this.i18n(c.label);
+            const encodedValue = [macroType, tokenId, c.id].join(this.delimiter);
+            const cssClass = actor.effects.entries.some(e => e.data.flags.core?.statusId === c.id) ? 'active' : '';
+            const image = c.icon;
+            const action = {name: name, id: c.id, encodedValue: encodedValue, img: image, cssClass: cssClass}
+
+            conditions.actions.push(action);
+        });
+
+        this._combineSubcategoryWithCategory(category, this.i18n('tokenactionhud.conditions'), conditions);
+    }
+	
+	 /** @private */
+    _addMultiConditions(list, tokenId) {
+        const category = this.initializeEmptyCategory('conditions');
+        const macroType = 'condition';
+
+        const availableConditions = CONFIG.statusEffects.filter(condition => condition.id !== '');
+        const actors = canvas.tokens.controlled.filter(t => !!t.actor).map(t => t.actor);
+
+        if (!availableConditions)
+            return;
+
+        let conditions = this.initializeEmptySubcategory();
+
+        availableConditions.forEach(c => {
+            const name = this.i18n(c.label);
+            const encodedValue = [macroType, tokenId, c.id].join(this.delimiter);
+            const cssClass = actors.every(actor => actor.effects.entries.some(e => e.data.flags.core?.statusId === c.id)) ? 'active' : '';
+            const image = c.icon;
+            const action = {name: name, id: c.id, encodedValue: encodedValue, img: image, cssClass: cssClass}
+
+            conditions.actions.push(action);
+        });
+
+        const conName = this.i18n('tokenactionhud.conditions');
+        this._combineSubcategoryWithCategory(category, conName, conditions);
+        this._combineCategoryWithList(list, conName, category);
+    }
+	/** END CONDITIONS **/
+	
     /** @private */
     _buildItem(tokenId, actor, macroType, item) {
         let encodedValue = [macroType, tokenId, item._id].join(this.delimiter);
