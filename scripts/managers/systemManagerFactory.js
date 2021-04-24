bee6a7aa1a7afc491f2d08b6a9ee8196ab52a871
pwatson100 <paul@skynet2020.co.uk>
2021-04-24 18:12:19 +0100

    Working for Characters, Synthetics and Creatures.

--- scripts/managers/systemManagerFactory.js
+++ scripts/managers/systemManagerFactory.js
@@ -13,40 +13,43 @@ import { SwadeSystemManager } from './swade.js';
 import { StarWarsFFGSystemManager } from './starwarsffg.js';
 import { Tormenta20SystemManager } from './tormenta20.js';
 import { SymbaroumSystemManager } from './symbaroum.js';
+import { AlienrpgSystemManager } from './alienrpg.js';
 
 export class SystemManagerFactory {
-    static create(system, appName) {
-        switch(system) {
-            case 'blades-in-the-dark':
-                return new BitDSystemManager(appName);
-            case 'demonlord':
-                return new DemonlordSystemManager(appName);
-            case 'dnd5e':
-                return new Dnd5eSystemManager(appName);
-            case 'dungeonworld':
-                return new DungeonWorldSystemManager(appName);
-            case 'pf1':
-                return new Pf1SystemManager(appName);
-            case 'D35E':
-                return new D35ESystemManager(appName);
-            case 'pf2e':
-                return new Pf2eSystemManager(appName);
-            case 'sfrpg':
-                return new SfrpgSystemManager(appName);
-            case 'sw5e':
-                return new Sw5eSystemManager(appName);
-            case 'wfrp4e':
-                return new Wfrp4eSystemManager(appName);
-            case 'lancer':
-                return new LancerSystemManager(appName);
-            case 'swade':
-                return new SwadeSystemManager(appName);
-            case 'starwarsffg':
-                return new StarWarsFFGSystemManager(appName);
-			      case 'tormenta20':
-				        return new Tormenta20SystemManager(appName);
-            case 'symbaroum':
-                return new SymbaroumSystemManager(appName);
-        }
+  static create(system, appName) {
+    switch (system) {
+      case 'blades-in-the-dark':
+        return new BitDSystemManager(appName);
+      case 'demonlord':
+        return new DemonlordSystemManager(appName);
+      case 'dnd5e':
+        return new Dnd5eSystemManager(appName);
+      case 'dungeonworld':
+        return new DungeonWorldSystemManager(appName);
+      case 'pf1':
+        return new Pf1SystemManager(appName);
+      case 'D35E':
+        return new D35ESystemManager(appName);
+      case 'pf2e':
+        return new Pf2eSystemManager(appName);
+      case 'sfrpg':
+        return new SfrpgSystemManager(appName);
+      case 'sw5e':
+        return new Sw5eSystemManager(appName);
+      case 'wfrp4e':
+        return new Wfrp4eSystemManager(appName);
+      case 'lancer':
+        return new LancerSystemManager(appName);
+      case 'swade':
+        return new SwadeSystemManager(appName);
+      case 'starwarsffg':
+        return new StarWarsFFGSystemManager(appName);
+      case 'tormenta20':
+        return new Tormenta20SystemManager(appName);
+      case 'symbaroum':
+        return new SymbaroumSystemManager(appName);
+      case 'alienrpg':
+        return new AlienrpgSystemManager(appName);
     }
+  }
 }
